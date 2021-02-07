8c6479b52448c64ef38483ab82e010bf217c06e2
Wilson0925 <54805745+Wilson0925@users.noreply.github.com>
2021-02-06 21:57:44 -0800

    Add files via upload

---
+++ p2init/libs/cmake-build-debug/CMakeFiles/Mythread.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Mythread.dir/queue.c.o"
+  "Mythread"
+  "Mythread.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Mythread.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
