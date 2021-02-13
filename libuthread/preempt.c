8e3cfdf66088dbd70a7830a11e60ebf5763e6542
Sparks <56856447+Sparksssj@users.noreply.github.com>
2021-02-12 19:28:28 -0800

    Add files via upload

--- libuthread/preempt.c
+++ libuthread/preempt.c
@@ -21,45 +21,51 @@ sigset_t signal_sets;
 
 void termination_handler ()
 {
+    // When timer reached, call yield to next ready thread.
     uthread_yield();
 }
 
 void preempt_start(void)
 {
-	/* TODO */
-	signal_handler.sa_handler = termination_handler;
+	signal_handler.sa_handler = termination_handler
+    // Set the mask to include SIGVTALRM only
     sigemptyset(&signal_sets);
     sigaddset(&signal_sets, SIGVTALRM);
     signal_handler.sa_mask = signal_sets;
+    // Set the signal handler
     sigaction(SIGVTALRM, &signal_handler, &signal_handler_restore);
+    // Set the time between now and the first interrupt by 10hz
 	timer.it_interval.tv_sec = 0;
 	timer.it_interval.tv_usec = 1000/HZ;
+	// Set the time between successive interrupt by 10hz
 	timer.it_value.tv_sec = 0;
 	timer.it_value.tv_usec = 1000/HZ;
+	// Set the alarm
 	setitimer(ITIMER_VIRTUAL, &timer, &timer_restore);
 }
 
 void preempt_stop(void)
 {
-	/* TODO */
+    // Disable the alarm/timer
     timer.it_interval.tv_sec = 0;
     timer.it_interval.tv_usec = 0;
     timer.it_value.tv_sec = 0;
     timer.it_value.tv_usec = 0;
     setitimer(ITIMER_VIRTUAL, &timer, NULL);
+    // Restore previous timer, signal handler configuration
     signal_handler = signal_handler_restore;
     timer = timer_restore;
 }
 
 void preempt_enable(void)
 {
-	/* TODO */
+    // unblock the mask
 	sigprocmask(SIG_UNBLOCK, &signal_sets, NULL);
 }
 
 void preempt_disable(void)
 {
-	/* TODO */
+	// block the mask
     sigprocmask(SIG_BLOCK, &signal_sets, NULL);
 }
 
